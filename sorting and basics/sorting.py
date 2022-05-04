521ece5f132dd930353e94acec0b3bc3745a3b0b
Shakun Kumar <69691796+shakunkumargithub@users.noreply.github.com>
2022-05-04 18:00:03 +0530

    Update sorting.py

--- sorting and basics/sorting.py
+++ sorting and basics/sorting.py
@@ -54,7 +54,7 @@ def selectionsort(a):
     """
     for i in range(len(a)):
         min = i
-        for j in range(i,len(a)):
+        for j in range(i,len(a)): /*04.05.2022 Shakun Kumar 'i' -> 'i+1' */
             if a[j] < a[min]: 
                 min = j
         a[i],a[min] = a[min], a[i]
